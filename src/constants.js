7a905d54a80232e824403760f0a0733b7c4e822d
Nina-gu <lotusgu9@gmail.com>
2020-11-08 12:23:44 -0600

    spaceX3

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const SAT_API_KEY = "YOUR KEY";
+export const SAT_API_KEY = "X6VTZD-ZFF4DU-4CQ2C9-4KZW";
 export const STARLINK_CATEGORY = "52";
 export const NEARBY_SATELLITE = "rest/v1/satellite/above";
 export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
